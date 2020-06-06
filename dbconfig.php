d4a4c7601a8f278fa02b16d1ec405e336ce78285
Emmanuel <emmaqm98@gmail.com>
2020-06-06 16:32:41 -0300

    fix config

--- dbconfig.php
+++ dbconfig.php
@@ -4,11 +4,12 @@ $DB_host = getenv('HOST');
 $DB_user = getenv('USER');
 $DB_pass = getenv('PASS');
 $DB_name = getenv('DB');
+$DB_port = getenv('PORT');
 
 
 try
 {
-	$DB_con = new PDO("mysql:host={$DB_host};dbname={$DB_name}",$DB_user,$DB_pass);
+	$DB_con = new PDO("mysql:host={$DB_host};port={$DB_port};dbname={$DB_name}",$DB_user,$DB_pass);
 	$DB_con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$DB_con->exec("set names utf8");
 }
