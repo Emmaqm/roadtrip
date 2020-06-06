318cd228ab34e3dff8da6b7ed2fc1ade8b52efed
Emmanuel <emmaqm98@gmail.com>
2020-06-06 16:26:49 -0300

    db config

---
+++ dbconfig.php
@@ -0,0 +1,25 @@
+<?php
+
+$DB_host = getenv('HOST');
+$DB_user = getenv('USER');
+$DB_pass = getenv('PASS');
+$DB_name = getenv('DB');
+
+
+try
+{
+	$DB_con = new PDO("mysql:host={$DB_host};dbname={$DB_name}",$DB_user,$DB_pass);
+	$DB_con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$DB_con->exec("set names utf8");
+}
+catch(PDOException $e)
+{
+	echo $e->getMessage();
+}
+
+include_once 'class_lib.php';
+
+$crud = new crud($DB_con);
+$ciudadesClass = new ciudadesClass($DB_con);
+
+?>
